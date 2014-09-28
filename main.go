d19888d7875103d640c85b1f67cfb88cf43c8473
Sylvain Colomer <sylvain.colomer@gmail.com>
2014-09-28 13:39:17 +0200

    Support DOCKER_HOST environement variable.

--- main.go
+++ main.go
@@ -10,14 +10,28 @@ import (
 	"encoding/json"
 	_ "reflect"
 	"text/template"
+	"strings"
+	"errors"
 )
 
-func main() {
-	req, err := http.NewRequest("GET", "/containers/2a03159ba971/json", nil)
+func Dial()(net.Conn, error) {
+	url := os.Getenv("DOCKER_HOST")
+	if (url == "") {
+		return net.Dial("unix", "/var/run/docker.sock")
+	}
 
-	// conn, err := net.Dial("unix", "/var/run/docker.sock")
-	conn, err := net.Dial("tcp", "tethys:2375")
-        if err != nil {
+	if (strings.HasPrefix(url, "tcp://")) {
+		return net.Dial("tcp", url[6:len(url)])
+	}
+
+	return nil, errors.New("Unhandled form : " + url)
+}
+
+func main() {
+	req, err := http.NewRequest("GET", "/containers/etcd/json", nil)
+
+	conn, err := Dial()
+  if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
@@ -28,12 +42,12 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	
+
 	body, _ := ioutil.ReadAll(resp.Body)
 	var f interface{}
-	
+
 	json.Unmarshal(body, &f)
-	
+
 	tmpl,_ := template.New("test").Parse("value={{(index (index .NetworkSettings.Ports \"4001/tcp\") 0).HostPort}}\n")
 	tmpl.Execute(os.Stdout, f)
 }
